0db8649ebd278ce347c11ed795a4b96cde40c075
monika <monikaglier@gmail.com>
2015-10-06 11:12:22 +0200

    add modules ApplicationControllerMethods

--- lib/jump_in/persistence/cookies.rb
+++ lib/jump_in/persistence/cookies.rb
@@ -7,14 +7,7 @@ module JumpIn
         klass.jumpin_callback :on_login,         :set_user_cookies
         klass.jumpin_callback :on_logout,        :remove_user_cookies
         klass.jumpin_callback :get_current_user, :current_user_from_cookies
-        klass::JUMPIN_CONTROLLER.class_eval do
-          def current_user_from_cookies
-            return nil unless cookies.signed[:jump_in_id] &&
-                              cookies.signed[:jump_in_class]
-            klass = cookies.signed[:jump_in_class].constantize
-            klass.find_by(id: cookies.signed[:jump_in_id])
-          end
-        end
+        klass::JUMPIN_CONTROLLER.include ApplicationControllerMethods
       end
 
       def set_user_cookies(user:)
@@ -28,6 +21,15 @@ module JumpIn
       def remove_user_cookies
         cookies.delete :jump_in_class
         cookies.delete :jump_in_id
+      end
+
+      module ApplicationControllerMethods
+        def current_user_from_cookies
+          return nil unless cookies.signed[:jump_in_id] &&
+                            cookies.signed[:jump_in_class]
+          klass = cookies.signed[:jump_in_class].constantize
+          klass.find_by(id: cookies.signed[:jump_in_id])
+        end
       end
     end
   end
